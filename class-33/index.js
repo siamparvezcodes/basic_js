aea9e1788ec8d8aa2b4af35236c1ae5dc121c603
siamparvez001 <siamparvez003@gmail.com>
2026-06-25 02:35:06 +0600

    complete conditional

---
+++ class-33/index.js
@@ -0,0 +1,25 @@
+const x = 5
+console.log(x==5)
+console.log(x===5)
+console.log(x=="5")
+console.log(x==="5")
+console.log(x==6)
+console.log(x!==6)
+console.log(x!==6)
+console.log(x!=="6")
+console.log(x!==5)
+console.log(x!=="5")
+
+
+let age = 18;
+if(age>=18){
+    console.log("You are adult")
+}
+else{
+    console.log("You are a young")
+}
+
+
+// Ternary Operator
+let age = 20
+console.log(age < 18 ? "Too young" : "You are adult")
