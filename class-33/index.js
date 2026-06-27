d709dbd1485131efafe7678fe92b14facca7986a
siamparvez001 <siamparvez003@gmail.com>
2026-06-27 23:28:35 +0600

    complete 32 no js basic class

--- class-33/index.js
+++ class-33/index.js
@@ -1,25 +1,38 @@
-const x = 5
-console.log(x==5)
-console.log(x===5)
-console.log(x=="5")
-console.log(x==="5")
-console.log(x==6)
-console.log(x!==6)
-console.log(x!==6)
-console.log(x!=="6")
-console.log(x!==5)
-console.log(x!=="5")
+// const x = 5
+// console.log(x==5)
+// console.log(x===5)
+// console.log(x=="5")
+// console.log(x==="5")
+// console.log(x==6)
+// console.log(x!==6)
+// console.log(x!==6)
+// console.log(x!=="6")
+// console.log(x!==5)
+// console.log(x!=="5")
 
 
-let age = 18;
-if(age>=18){
-    console.log("You are adult")
-}
-else{
-    console.log("You are a young")
-}
+// let age = 18;
+// if(age>=18){
+//     console.log("You are adult")
+// }
+// else{
+//     console.log("You are a young")
+// }
 
 
 // Ternary Operator
-let age = 20
-console.log(age < 18 ? "Too young" : "You are adult")
+// let age = 20
+// console.log(age < 18 ? "Too young" : "You are adult")
+
+
+// let age = "18"
+// age = Number(age)
+// if (isNaN(age)) {
+//     console.log("Enter a valid age number")
+// }
+// else {
+//     console.log(age < 18 ? "Too young " : "You are adult")
+// }
+
+
+
---
+++ class-34/index.js
@@ -0,0 +1,7 @@
+const person = { fname: "Siam", lname: "Parvez", age: 20 }
+
+for(let i in person){
+    console.log(person[i])
+}
+
+
