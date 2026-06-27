cc6d7f9d7ac47db639998157ddb466cd92129e0e
siamparvez001 <siamparvez003@gmail.com>
2026-06-28 01:45:18 +0600

    explore js maps

---
+++ class-42/index.js
@@ -0,0 +1,12 @@
+const fruits = new Map([
+    ['apple', 500],
+    ['banana', 700],
+    ['orange', 900]
+])
+console.log(fruits)
+console.log(fruits.delete('apple'))
+
+const person = {
+    fname: "Siam"
+}
+person['fname']
