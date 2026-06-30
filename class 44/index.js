900ef06dbcbfd34dbca4238c026dd858b989d919
siamparvez001 <siamparvez003@gmail.com>
2026-06-30 12:00:57 +0600

    complete 44 no class

---
+++ class 44/index.js
@@ -0,0 +1,32 @@
+console.log(Number(""))
+console.log(Number(" "))
+console.log(Number("5.5"))
+console.log(Number("55 66"))
+console.log(parseFloat("4.7"))
+
+
+
+// The unary + operator
+let x = "6"
+let y = + x
+console.log(y)
+
+// number to string
+console.log(String(7 + 8))
+
+let a = 10.5
+console.log(a.toFixed())
+console.log(a.toExponential())
+
+
+// boolean to string
+console.log(false)
+console.log(true)
+
+
+
+console.log("5" + 2)
+console.log("Siam" + 2)
+
+console.log("5" - 2)
+console.log("Siam" - 2)
---
+++ class-43/index.js
@@ -0,0 +1,28 @@
+let a = "Bangladesh"
+let b = 71
+let c = a + " " + b
+console.log(typeof c)
+console.log(typeof function () { })
+console.log(typeof b)
+console.log(typeof a)
+console.log(typeof NaN)
+console.log(typeof undefined)
+console.log(typeof car)
+console.log(typeof null)
+console.log(typeof true)
+console.log(typeof false)
+console.log(typeof [1, 2, 3])
+console.log(typeof { name: 'Siam', age: 20 })
+
+// typeof is a operator , it is not s data type.
+
+// data type
+// 1. Primitive data type
+// string
+// boolean
+// number
+// number
+// 2. Non primitive data type
+// object
+// array
+// function 
