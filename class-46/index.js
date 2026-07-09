d0d49c821cbc74f67eb048b08fef756794e30659
siamparvez001 <siamparvez003@gmail.com>
2026-07-10 04:13:21 +0600

    complete class no 46

---
+++ class-46/index.js
@@ -0,0 +1,10 @@
+// Regular Expressions
+
+let test = "Visit Microsoft powered by microsoft";
+let n = test.replace(/Microsoft/i, "W3school")
+console.log(n) 
+let l = test.replace(/Microsoft/ig, "W3school")
+console.log(l) 
+let text = "Visit W3schools!";
+let m = text.search(/W3schools/i)
+console.log(m) 
