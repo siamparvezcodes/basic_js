53553089148f2f04aa52ba52211c06515fade8cd
siamparvez001 <siamparvez003@gmail.com>
2026-07-10 02:29:45 +0600

    complete class no 45

---
+++ class-45/index.js
@@ -0,0 +1,15 @@
+console.log((5).toString(2))
+console.log((5).toString())
+console.log((5.6).toString(2))
+console.log((10).toString(2))
+
+
+//  Regular Expressions
+let text = "Visit W3schools!";
+let x= text.search(/w3school/i);
+let y= text.search("W3school");
+console.log(x)
+console.log(y)
+
+console.log((10).toString(16))
+console.log(110 & 001)
