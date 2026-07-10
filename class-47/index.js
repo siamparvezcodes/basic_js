336801bc495b1e08da1f2d8ae0a6cb317eafbc96
siamparvez001 <siamparvez003@gmail.com>
2026-07-10 22:47:38 +0600

    complete 47 no. class

--- class-47/index.js
+++ class-47/index.js
@@ -1,1 +1,21 @@
-console.log("siam parvez")
+
+
+try{
+    adddlert("Welcome to JavaScript Errors")
+}
+catch(err){
+    document.getElementById("demo").innerHTML = err.message;
+}
+
+let x = 12;
+try {
+    if (x == "") throw "empty";
+    if (isNaN(x)) throw "not a number";
+    if (x < 5) throw "too low";
+    if (x > 10) throw "too high";
+}
+catch (err) {
+    // message.innerHTML = "Input is " + err;
+    console.log("Input is " + err);
+}
+console.log("JavaScript Errors") 
