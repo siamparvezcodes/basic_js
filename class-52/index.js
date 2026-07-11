eb14a9891f9809addb345dc1634d5418e12f3a9f
siamparvez001 <siamparvez003@gmail.com>
2026-07-12 05:17:06 +0600

    learn arrow function

---
+++ class-52/index.js
@@ -0,0 +1,13 @@
+console.log("Arrow Function")
+
+// Before
+const hello1 = function () {
+    return "Hello World"
+}
+
+// With arrow funciton
+const hello2 = () => {
+    return "Hello World"
+}
+
+const hello3 = () => "Hellow World"
